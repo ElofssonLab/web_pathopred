93715ce9c034e1fa0286c5cb7782133f3bb0f589
akvakv <akv@kth.se>
2018-12-06 21:14:30 +0100

    Django 1.11.15 LTS and lxml upgrade

--- proj/urls.py
+++ proj/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 from proj import views
 from proj import pred
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'proj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -14,5 +14,7 @@ urlpatterns = patterns('',
     url(r'^pred/', include('pred.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/pred/login/'}),
+    url(r'^pred/', include('proj.pred.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')), # new
     url(r'^$', views.home, name='home'),
-)
+]
